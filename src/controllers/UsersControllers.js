c7fa8c6143e21d722e4390990f6b57e50246823f
Leon Martins <leonellan12@gmail.com>
2023-11-01 10:30:00 -0300

    add tratamento de erro

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/appError")
+
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório")
+    }
 
     response.status(201).json({ name, email, password })
   }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+require("express-async-errors");
+
+const AppError = require("./utils/appError");
+
 const express = require("express");
 const routes = require("./routes");
 
@@ -6,6 +10,21 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((error, req, res, next) => {
+  if(error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
