3615439abc25e6fa96c6d848a98ebd6b18b64fb6
Leon Martins <leonellan12@gmail.com>
2023-11-08 09:09:20 -0300

    add routes from moviesController

---
+++ src/routes/movies.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+
+const MoviesController = require("../controllers/MoviesController")
+
+const moviesRoutes = Router()
+
+const moviesControllers = new MoviesController();
+
+
+moviesRoutes.post("/:user_id", moviesControllers.create)
+
+
+module.exports = moviesRoutes
